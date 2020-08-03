f865ce07dc5f99a96222660e14b6d7ce8e4fc778
edithntinyari <emothegi@gmail.com>
2020-08-03 09:21:04 +0300

    added changes in supplier_activity.java

---
+++ app/src/main/java/Supplier_activity.java
@@ -0,0 +1,71 @@
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.moringaschool.mybussinessapp.R;
+
+public class Supplier_activity extends AppCompatActivity {
+
+
+
+
+    public class MainActivity extends AppCompatActivity {
+
+        EditText SupplierfirstName;
+        EditText SupplierlastName;
+        EditText address;
+        EditText email;
+        Button register;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            SupplierfirstName = findViewById(R.id.SupplierfirstName);
+            SupplierlastName = findViewById(R.id.SupplierlastName);
+            address = findViewById(R.id.address);
+            email = findViewById(R.id.email);
+            register = findViewById(R.id.register);
+
+            register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    checkDataEntered();
+                }
+            });
+        }
+
+        boolean isEmail(EditText text) {
+            CharSequence email = text.getText().toString();
+            return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        }
+
+        boolean isEmpty(EditText text) {
+            CharSequence str = text.getText().toString();
+            return TextUtils.isEmpty(str);
+        }
+
+        void checkDataEntered() {
+            if (isEmpty(SupplierfirstName)) {
+                Toast t = Toast.makeText(this, "You must enter first name to register!", Toast.LENGTH_SHORT);
+                t.show();
+            }
+
+            if (isEmpty(SupplierlastName)) {
+                SupplierlastName.setError("Last name is required!");
+            }
+
+            if (isEmail(email) == false) {
+                email.setError("Enter valid email!");
+            }
+
+        }
+    }
+
+}
