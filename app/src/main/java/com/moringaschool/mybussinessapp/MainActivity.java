12a81a5afb3b20c341fddcfb350a2dcc2d030c40
edithntinyari <emothegi@gmail.com>
2020-08-03 08:59:19 +0300

    added register button

--- app/src/main/java/com/moringaschool/mybussinessapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/mybussinessapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.moringaschool.mybussinessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mFindGoodsButton = (Button) findViewById(R.id.bottom);
+        View mFindRestaurantsButton = null;
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "FINDGOODS", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
